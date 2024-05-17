f91b988fb970352507bdbc7509f69b43adf59c33
Anand <83487595+Anand6109@users.noreply.github.com>
2024-05-17 14:33:56 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <nav>Complete your daily tasks here!!</nav>
+  )
+}
+
+export default Header
---
+++ src/components/Home.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import Task from './Task';
+
+const Home = () => {
+
+    // Add error handling for JSON parsing
+    const getInitialTasks = () => {
+        try {
+            const tasks = localStorage.getItem("tasks");
+            return tasks ? JSON.parse(tasks) : [];
+        } catch (error) {
+            console.error("Error parsing JSON from localStorage", error);
+            return [];
+        }
+    };
+
+    const [tasks, setTasks] = useState(getInitialTasks());
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setTasks([...tasks, { title, description }]);
+        setTitle(""); // Clear the input fields after submission
+        setDescription(""); // Clear the input fields after submission
+    };
+
+    const deleteTask = (index) => {
+        const filteredArr = tasks.filter((val, i) => i !== index);
+        setTasks(filteredArr);
+    };
+
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
+
+    return (
+        <div className="container">
+            <h1>Daily tasks</h1>
+            <form onSubmit={submitHandler}>
+                <input
+                    type="text"
+                    placeholder='Title'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea
+                    placeholder='Description'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+                <button type="submit">Add</button>
+            </form>
+
+            {tasks.map((item, index) => (
+                <Task
+                    key={index}
+                    title={item.title}
+                    description={item.description}
+                    deleteTask={deleteTask}
+                    index={index}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Home;
